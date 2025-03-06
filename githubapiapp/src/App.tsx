866462955c88bb23690e6e37246680055e31abad
LeeJinHyuk9358 <leejh9358@naver.com>
2025-03-06 21:43:35 +0900

    feat: 20250306 weather / github api uploaded

--- githubapiapp/src/App.tsx
+++ githubapiapp/src/App.tsx
@@ -1,12 +1,29 @@
 import { useState } from 'react'
+import axios from 'axios';
 import './App.css'
 
+type Repository = {
+  id: number;
+  full_name: string;
+  html_url: string;
+};
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [ keyword, setKeyword ] = useState('');
+  const [ repodata, setRepodata ] = useState<Repository[]>([]);
+
+  const handleClick = () => {
+    //REST API호출 로직 구현
+  }
+
 
   return (
     <>
-      
+      <input
+        value={keyword}
+        onChange={e => setKeyword(e.target.value)}
+      /><br /> <br />
+      <button onClick={handleClick}>Fetch</button>
     </>
   )
 }
