61089f6a43deb78f9167d20cd17e727d324683c5
LeeJinHyuk9358 <leejh9358@naver.com>
2025-03-07 21:50:02 +0900

    feat: 20250307 ag grid with an error

--- githubapiapp/src/App.tsx
+++ githubapiapp/src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import axios from 'axios';
-import './App.css'
+import './App.css';
 
 type Repository = {
   id: number;
@@ -13,17 +13,36 @@ function App() {
   const [ repodata, setRepodata ] = useState<Repository[]>([]);
 
   const handleClick = () => {
-    //REST API호출 로직 구현
+    axios.get<{ items: Repository[]}>(`https://api.github.com/search/repositories?q=${keyword}`)
+    .then(response => setRepodata(response.data.items))
+    .catch(err => console.log(err))
   }
-
 
   return (
     <>
       <input
         value={keyword}
         onChange={e => setKeyword(e.target.value)}
-      /><br /> <br />
+        placeholder='python_study_hard'
+      />
+      <br /><br />
       <button onClick={handleClick}>Fetch</button>
+      {repodata.length === 0 ? (
+        <p>접근 가능한 데이터가 없습니다</p>
+      ):(
+        <table>
+          <tbody>
+            {repodata.map(repo => (
+              <tr key={repo.id}>
+                <td>{repo.full_name}</td>
+                <td>
+                  <a href={repo.html_url}>{repo.html_url}</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   )
 }
